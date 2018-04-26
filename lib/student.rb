44fa7378bf1f25e3c6f2916e2dcfed4ae227ef47
Vanessa Toro <vanetoro84@gmail.com>
2018-04-26 15:46:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_table
       sql = <<-SQL
-        CREATE TABLE student(
+        CREATE TABLE students(
           id INTEGER PRIMARY KEY,
           name TEXT,
           grade TEXT
