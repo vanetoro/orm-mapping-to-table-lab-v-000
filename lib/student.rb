9b87ced2da6131a8405ceef6f159aba5507fa27a
Vanessa Toro <vanetoro84@gmail.com>
2018-04-26 15:58:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
     DB[:conn].execute("DROP TABLE students")
   end
 
-  def self.save
+  def save
       DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?,?)", self.name, self.grade)
   end
 
